ad2644d285459d3494e7cbf3b651d80bba17ec7d
sujang958 <enter7377@naver.com>
2021-07-24 16:03:21 +0900

    feat(database): set-up database code

---
+++ src/database/index.ts
@@ -0,0 +1,62 @@
+import Mongoose from "mongoose"
+import { config } from "dotenv"
+
+config()
+
+interface IToken {
+    user: string
+    guild: string
+    token: string
+    created_time: number
+    exprise_time: number
+}
+
+interface IGuild {
+    id: string
+    role: string
+}
+
+export const connect = async () => {
+    await Mongoose.connect(String(process.env.DB_URI),  {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+    })
+}
+
+export const TokenSchema = new Mongoose.Schema<IToken>({
+    user: {
+        type: String,
+        required: true,
+    },
+    guild: {
+        type: String,
+        required: true,
+    },
+    token: {
+        type: String,
+        required: true,
+    },
+    created_time: {
+        type: Number,
+        required: true,
+    },
+    exprise_time: {
+        type: Number,
+        required: true,
+    },
+})
+
+export const TokenModel = Mongoose.model<IToken>('token', TokenSchema)
+
+export const GuildSchema = new Mongoose.Schema<IGuild>({
+    id: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        required: true,
+    }
+})
+
+export const GuildModel = Mongoose.model<IGuild>('guild', GuildSchema)
